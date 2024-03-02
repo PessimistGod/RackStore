34b742bfaa3dc5a25a508b6211e235b78abaaa0b
PessimistGod <rameshrahul0004@gmail.com>
2024-03-02 15:43:27 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 
 const corsOptions = {
-    origin: [ "https://rack-store-frontend-three.vercel.app", "https://rack-store-backend-ten.vercel.app",  "http://localhost:3000"],
+    origin: [ "https://rack-store-frontend-three.vercel.app", "https://rack-store-backend-ten.vercel.app",  "http://localhost:3000","https://rack-store-frontend.vercel.app","https://rack-store-backend.vercel.app"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
 };
